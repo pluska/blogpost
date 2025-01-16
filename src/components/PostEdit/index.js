ae708365c51bacc130ef4a5c8129287bec664f0e
Andres Pluska <pluskaandres@gmail.com>
2025-01-16 15:31:51 -0500

    Preparing for deploy

--- src/components/PostEdit/index.js
+++ src/components/PostEdit/index.js
@@ -4,111 +4,113 @@ import { Form, Input, Button, ButtonDelete } from "./style";
 import { toast } from "react-toastify";
 import { getPostsById, updatePost, deletePost } from "../../redux/services";
 import { useHistory } from "react-router";
-import { confirmAlert } from 'react-confirm-alert';
-import 'react-confirm-alert/src/react-confirm-alert.css';
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 export const PostEdit = (matchPath) => {
-  const match = matchPath.matchPath;
-  const [post, setPost] = useState("");
-  const history = useHistory()
-  const loadingPost = async () => {
-    const res = await getPostsById(match);
-    setPost(res.data);
-  };
-  useEffect(() => {
-    loadingPost();
-  }, [post]);
+    const match = matchPath.matchPath;
+    const [post, setPost] = useState("");
+    const history = useHistory();
+    const loadingPost = async () => {
+        const res = await getPostsById(match);
+        setPost(res.data);
+    };
+    useEffect(() => {
+        loadingPost();
+    }, [loadingPost]);
 
-  const handleInputChange = (e) => {
-    setPost({ ...post, [e.target.name]: e.target.value });
-  };
+    const handleInputChange = (e) => {
+        setPost({ ...post, [e.target.name]: e.target.value });
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const res = await updatePost(post);
-    console.log(res);
-    toast.success("Post Updated");
-    history.push('/admin/author')
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await updatePost(post);
+        console.log(res);
+        toast.success("Post Updated");
+        history.push("/admin/author");
+    };
 
-  const confirmDelete = async () => {
-    const res = await deletePost(post);
-    console.log(res);
-    toast.success("Post Deleted");
-    history.push('/admin/author')
-  };
+    const confirmDelete = async () => {
+        const res = await deletePost(post);
+        console.log(res);
+        toast.success("Post Deleted");
+        history.push("/admin/author");
+    };
 
-  const handleDelete = (e) => {
-    e.preventDefault();
-    confirmAlert({
-      title: 'WAIT',
-      message: 'Do you really want to delete this post?!',
-      buttons: [
-        {
-          label: 'Yes',
-          onClick: confirmDelete
-        },
-        {
-          label: 'No',
-          onClick: () => alert('Click No')
-        }
-      ]
-    });
-  };
+    const handleDelete = (e) => {
+        e.preventDefault();
+        confirmAlert({
+            title: "WAIT",
+            message: "Do you really want to delete this post?!",
+            buttons: [
+                {
+                    label: "Yes",
+                    onClick: confirmDelete,
+                },
+                {
+                    label: "No",
+                    onClick: () => alert("Click No"),
+                },
+            ],
+        });
+    };
 
-  
-
-  return (
-    <Grid container direction="row" justify="center">
-      <Form onSubmit={handleSubmit}>
-        <Typography variant="h3" style={{ textAlign: "center" }}>
-          Post Edition
-        </Typography>
-        <Typography variant="h6">Title</Typography>
-        <Grid container direction="row">
-          <Grid item xs={12}>
-            <Input
-              placeholder="title"
-              name="title"
-              value={post.title}
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <label />
-          <Input
-            type="hidden"
-            name="draft"
-            value={post.draft}
-            onChange={handleInputChange}
-          />
+    return (
+        <Grid container direction="row" justify="center">
+            <Form onSubmit={handleSubmit}>
+                <Typography variant="h3" style={{ textAlign: "center" }}>
+                    Post Edition
+                </Typography>
+                <Typography variant="h6">Title</Typography>
+                <Grid container direction="row">
+                    <Grid item xs={12}>
+                        <Input
+                            placeholder="title"
+                            name="title"
+                            value={post.title}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <label />
+                    <Input
+                        type="hidden"
+                        name="draft"
+                        value={post.draft}
+                        onChange={handleInputChange}
+                    />
+                </Grid>
+                <Grid>
+                    <Typography variant="h6">Content</Typography>
+                    <Input
+                        name="body"
+                        value={post.body}
+                        onChange={handleInputChange}
+                    />
+                    <Typography variant="h6">Publish Date</Typography>
+                    <Input
+                        placeholder="publish_date"
+                        type="date"
+                        value={post.publish_Date}
+                        name="publish_date"
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        type="hidden"
+                        name="author_firstname"
+                        value={post.author_firstname}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        type="hidden"
+                        name="author_id"
+                        value={post.author_id}
+                        onChange={handleInputChange}
+                    />
+                </Grid>
+                <Button>Update</Button>
+                <ButtonDelete onClick={handleDelete}>Delete</ButtonDelete>
+            </Form>
         </Grid>
-        <Grid>
-          <Typography variant="h6">Content</Typography>
-          <Input name="body" value={post.body} onChange={handleInputChange} />
-          <Typography variant="h6">Publish Date</Typography>
-          <Input
-            placeholder="publish_date"
-            type="date"
-            value={post.publish_Date}
-            name="publish_date"
-            onChange={handleInputChange}
-          />
-          <Input
-            type="hidden"
-            name="author_firstname"
-            value={post.author_firstname}
-            onChange={handleInputChange}
-          />
-          <Input
-            type="hidden"
-            name="author_id"
-            value={post.author_id}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Button>Update</Button>
-        <ButtonDelete onClick={handleDelete}>Delete</ButtonDelete>
-      </Form>
-    </Grid>
-  );
+    );
 };
